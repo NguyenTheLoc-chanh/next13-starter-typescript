46346fb8d8550f3e19fb2870c4ed7f818415267e
NguyenTheLoc-chanh <22a1001d0199@students.hou.edu.com>
2024-09-14 23:15:00 +0700

    Reder Data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,17 +6,32 @@ import x from '@/app/style/app.module.css'
 import y from '@/app/style/main.module.css'
 import AppTable from '@/components/app.table';
 import { useEffect } from 'react'
+import useSWR from "swr";
 
 export default function Home() {
 
-  useEffect(() => {
-    const fetchData = async() =>{
-      const res = await fetch(" http://localhost:8000/blogs");
-      const data = await res.json();
-      console.log(data)
+  const fetcher = (url: string) => fetch(url).then((res) => res.json());
+  const { data, error, isLoading } = useSWR(
+    "http://localhost:8000/blogs",
+    fetcher,
+    {
+      revalidateIfStale: false,
+      revalidateOnFocus: false,
+      revalidateOnReconnect: false
     }
-    fetchData();
-  })
+  );
+  if(!data){
+    return <div>Loading.....</div>
+  }
+
+  // useEffect(() => {
+  //   const fetchData = async() =>{
+  //     const res = await fetch(" http://localhost:8000/blogs");
+  //     const data = await res.json();
+  //     console.log(data)
+  //   }
+  //   fetchData();
+  // })
   return (
     <div>
       <li>
@@ -29,7 +44,9 @@ export default function Home() {
         <Link href={"/Tiktok"}>Tiktok</Link>
       </li>
       <div>
-        <AppTable/>
+        <AppTable
+        blogs={data}
+        />
       </div>
     </div>
   )
---
+++ src/components/app.table.tsx
@@ -0,0 +1,43 @@
+'use client'
+
+import Table from 'react-bootstrap/Table';
+import { Button } from 'react-bootstrap';
+import { Interface } from 'readline';
+
+interface IProps {
+  blogs: IBlog[]
+}
+const AppTable = (props: IProps) => {
+  const {blogs} = props;
+  console.log("check",props);
+    return (
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>No</th>
+              <th>Title</th>
+              <th>Author</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {blogs?.map(blog => {
+              return <tr key = {blog.id}>
+                <td>{blog.id}</td>
+                <td>{blog.title}</td>
+                <td>{blog.author}</td>
+                <td>
+                  <Button>View</Button>
+                  <Button variant='warning' className='mx-3'>Edit</Button>
+                  <Button variant='danger'>Delete</Button>
+                </td>
+              </tr>
+            })}
+            
+            
+          </tbody>
+        </Table>
+    );
+}
+
+export default AppTable;
